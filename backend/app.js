0ec016ce985c049445396e19aae16b4ee57fdfe0
gerismumo <geraldmumo6@gmail.com>
2024-01-24 14:53:59 +0300

    cors changes

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,21 @@ const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
 dotenv.config({path: './database/.env'});
 
+const allowedOrigins = ['http://localhost:3000'];
 
-;
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '200mb' }));
 app.use(bodyParser.json({ limit: '200mb' }));
 app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
