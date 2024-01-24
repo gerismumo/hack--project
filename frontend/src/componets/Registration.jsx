3fe35dfce4f2fbe1b682f2c204947b2fdbbb826a
gerismumo <geraldmumo6@gmail.com>
2024-01-25 01:10:41 +0300

    added a payment button

--- frontend/src/componets/Registration.jsx
+++ frontend/src/componets/Registration.jsx
@@ -6,6 +6,10 @@ import { useNavigate } from 'react-router-dom';
 function Registration() {
   const API_URL = process.env.REACT_APP_API;
     const navigate = useNavigate();
+
+    const[openForm, setOpenForm] = useState(true);
+    const[openPay, setOpenPay] = useState(false);
+
     const handleClick = () => {
         navigate('/');
     }
@@ -50,8 +54,8 @@ function Registration() {
       })
         .then((response) => {
           if (response.ok) {
-            
-            navigate('/login');
+            setOpenForm(false);
+            setOpenPay(true);
           } else {
            
             setError("Registration failed. Please try again.");
@@ -59,95 +63,114 @@ function Registration() {
         });
   };
 
+  const handlePay = () => {
+    setOpenPay(false);
+    setOpenForm(true);
+    navigate('/login');
+  }
   return (
-    <div className="registration">
-        <header className="header">
-                <div className="log-area">
-                    <h2>Sea Trust Navigator</h2>
-                </div>
-                <div className="button-tabs">
-                    <button onClick={handleClick}>Home</button>
-                </div>
-            </header>
-            
-            <div className="reg-container">
-            <div className="registration-container">
-        <h2>Register Your Cooperative</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Company Name</label>
-          <input
-            type="text"
-            name="companyName"
-            value={formData.companyName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Type of Profile</label>
-          <select 
-           name="typeOfProfile"
-           value={formData.typeOfProfile}
-           onChange={handleChange}
-           >
-            <option value=""></option>
-            <option value="Ecommerce">E commerce Page</option>
-            <option value="information">Blogs Information</option>
-            <option value="information">Static websites</option>
-            <option value="information">Forums</option>
-            <option value="information">Social networks</option>
-            <option value="information">Portfolios</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Contact Number</label>
-          <input
-            type="tel"
-            name="contactNumber"
-            value={formData.contactNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-        <p> <p>If you have an account, you can <a href="/login">login here</a>.</p></p>
-      </form>
-        </div>
-            </div>
-        
-     
-    </div>
+    <>
+      <div className="registration">
+          <header className="header">
+              <div className="log-area">
+                  <h2>Sea Trust Navigator</h2>
+              </div>
+              <div className="button-tabs">
+                  <button onClick={handleClick}>Home</button>
+              </div>
+          </header>
+              <div className="reg-container">
+              
+                  <div className="registration-container">
+                {openForm && (
+                  <>
+                  <h2>Register Your Cooperative</h2>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label>Company Name</label>
+                      <input
+                        type="text"
+                        name="companyName"
+                        value={formData.companyName}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Email</label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Type of Profile</label>
+                      <select 
+                      name="typeOfProfile"
+                      value={formData.typeOfProfile}
+                      onChange={handleChange}
+                      >
+                        <option value=""></option>
+                        <option value="Ecommerce">E commerce Page</option>
+                        <option value="information">Blogs Information</option>
+                        <option value="information">Static websites</option>
+                        <option value="information">Forums</option>
+                        <option value="information">Social networks</option>
+                        <option value="information">Portfolios</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label>Password</label>
+                      <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Confirm Password</label>
+                      <input
+                        type="password"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Contact Number</label>
+                      <input
+                        type="tel"
+                        name="contactNumber"
+                        value={formData.contactNumber}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <button type="submit">Register</button>
+                    <p> <p>If you have an account, you can <a href="/login">login here</a>.</p></p>
+                  </form>
+                  </>
+                )}
+                {openPay && (
+                  <div className="">
+                    <p>Amount to pay: $99.99</p>
+                    <button onClick={handlePay}>Pay</button>
+                  </div>
+                )}
+                  </div>
+              
+              </div>
+          
+      
+      </div>
+    </>
+    
   );
 }
 
